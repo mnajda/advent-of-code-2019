92e25ea0c26c6fecf2bd181fb65752686b9e5267
Mateusz Najda <mateusznajda@gmail.com>
2019-12-28 11:11:57 +0100

    Added days 14 and 15

---
+++ day14/part2/src/main.rs
@@ -0,0 +1,127 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::env;
+use std::fs;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Recipe {
+    material: String,
+    quantity: i64,
+}
+
+fn parse_value(val: String) -> Recipe {
+    let split: Vec<String> = val
+        .split_whitespace()
+        .map(|item| item.to_string())
+        .collect();
+
+    let material: String = split.last().unwrap().clone();
+    let quantity: i64 = split.first().unwrap().clone().parse().unwrap();
+
+    return Recipe {
+        material: material,
+        quantity: quantity,
+    };
+}
+
+fn tokenize(filepath: &String) -> HashMap<String, (i64, Vec<Recipe>)> {
+    let contents = fs::read_to_string(filepath).expect("Error reading file");
+    let lines: Vec<&str> = contents.split('\n').collect();
+    let mut recipes: HashMap<String, (i64, Vec<Recipe>)> = HashMap::new();
+
+    for line in lines {
+        let split: Vec<&str> = line.split("=>").map(|item| item.trim()).collect();
+        let rhs = split.last().unwrap().to_string();
+        let key = parse_value(rhs);
+
+        let lhs: Vec<String> = split[0]
+            .split(',')
+            .map(|item| item.trim().to_string())
+            .collect();
+
+        let mut needed_materials: Vec<Recipe> = Vec::new();
+
+        for material in lhs {
+            needed_materials.push(parse_value(material));
+        }
+
+        recipes.insert(key.material.clone(), (key.quantity, needed_materials));
+    }
+
+    return recipes;
+}
+
+fn get_ore_quantity(quantity: i64, recipes: HashMap<String, (i64, Vec<Recipe>)>) -> i64 {
+    let mut supply: HashMap<String, i64> = HashMap::new();
+    let mut orders: VecDeque<Recipe> = VecDeque::new();
+    let mut ore_needed = 0;
+    orders.push_back(Recipe {
+        material: "FUEL".to_string(),
+        quantity: quantity,
+    });
+
+    while !orders.is_empty() {
+        let order = orders.pop_front().unwrap();
+        let is_in_supply = supply.get(&order.material) != None;
+
+        if order.material == "ORE" {
+            ore_needed += order.quantity;
+        } else if is_in_supply && order.quantity <= *supply.get(&order.material).unwrap() {
+            *supply.get_mut(&order.material).unwrap() -= order.quantity;
+        } else {
+            let amount = if is_in_supply {
+                order.quantity - *supply.get(&order.material).unwrap()
+            } else {
+                order.quantity
+            };
+            let recipe = recipes.get(&order.material).unwrap();
+            let batches = f64::ceil(amount as f64 / recipe.0 as f64) as i64;
+
+            for ingredient in &recipe.1 {
+                orders.push_back(Recipe {
+                    material: ingredient.material.clone(),
+                    quantity: ingredient.quantity * batches,
+                });
+            }
+
+            let leftover = batches * recipe.0 - amount;
+            supply.insert(order.material.clone(), leftover);
+        }
+    }
+
+    return ore_needed;
+}
+
+fn solve(recipes: HashMap<String, (i64, Vec<Recipe>)>) -> i64 {
+    let mut upper_bound = std::i64::MAX;
+    let mut lower_bound = 1;
+    let ore_capacity: i64 = 1000000000000;
+
+    while lower_bound + 1 != upper_bound {
+        let amount = if upper_bound == std::i64::MAX {
+            lower_bound * 2
+        } else {
+            (upper_bound + lower_bound) / 2
+        };
+        let ore_needed = get_ore_quantity(amount, recipes.clone());
+        if ore_needed > ore_capacity {
+            upper_bound = amount;
+        } else {
+            lower_bound = amount;
+        }
+    }
+
+    return lower_bound;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Provide filename");
+        return;
+    }
+    let filepath = &args[1];
+    let materials = tokenize(filepath);
+    let result = solve(materials);
+    println!("{}", result);
+}
---
+++ day15/part1/src/main.rs
@@ -0,0 +1,149 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::env;
+use std::fs;
+
+mod intcode;
+
+type Point = (i64, i64);
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Tile {
+    Wall,
+    Empty,
+    Oxygen,
+}
+
+fn tokenize(filepath: &String) -> Vec<i64> {
+    let contents = fs::read_to_string(filepath).expect("Error reading file");
+    return contents
+        .split(',')
+        .map(|input| input.parse().unwrap())
+        .collect();
+}
+
+fn unwrap_output(output: Option<i64>) -> i64 {
+    match output {
+        Some(item) => item,
+        None => panic!("Expected output value"),
+    }
+}
+
+fn get_tile_type(id: i64) -> Tile {
+    match id {
+        0 => Tile::Wall,
+        1 => Tile::Empty,
+        2 => Tile::Oxygen,
+        _ => panic!("Unexpected tile id"),
+    }
+}
+
+fn get_new_position(direction: i64, old_position: &Point) -> Point {
+    match direction {
+        1 => (old_position.0, old_position.1 + 1),
+        2 => (old_position.0, old_position.1 - 1),
+        3 => (old_position.0 + 1, old_position.1),
+        4 => (old_position.0 - 1, old_position.1),
+        _ => panic!("Unexpected direction"),
+    }
+}
+
+fn get_reverse_dir(direction: i64) -> i64 {
+    match direction {
+        1 => 2,
+        2 => 1,
+        3 => 4,
+        4 => 3,
+        _ => panic!("Unexpected direction"),
+    }
+}
+
+fn visit(machine: &mut intcode::Machine, map: &mut HashMap<Point, Tile>, pos: Point) {
+    for dir in 1..5 {
+        let new_pos = get_new_position(dir, &pos);
+
+        if !map.contains_key(&new_pos) {
+            machine.push_input(dir);
+            machine.execute_program();
+
+            let tile = get_tile_type(unwrap_output(machine.pop_output()));
+            map.insert(new_pos, tile);
+
+            if tile != Tile::Wall {
+                visit(machine, map, new_pos);
+
+                let reversed = get_reverse_dir(dir);
+
+                machine.push_input(reversed);
+                machine.execute_program();
+
+                let reversed_tile = get_tile_type(unwrap_output(machine.pop_output()));
+                let reversed_pos = get_new_position(dir, &pos);
+                if !map.contains_key(&reversed_pos) {
+                    map.insert(reversed_pos, reversed_tile);
+                }
+            }
+        }
+    }
+}
+
+fn generate_map(program: Vec<i64>) -> HashMap<Point, Tile> {
+    let mut machine = intcode::create_new_machine(program);
+    let mut map: HashMap<Point, Tile> = HashMap::new();
+
+    map.insert((0, 0), Tile::Empty);
+
+    visit(&mut machine, &mut map, (0, 0));
+
+    return map;
+}
+
+fn solve(map: HashMap<Point, Tile>) {
+    let mut to_visit: VecDeque<Point> = VecDeque::new();
+    let mut prev: HashMap<Point, Point> = HashMap::new();
+    let oxygen_pos = map.iter().find(|item| *item.1 == Tile::Oxygen).unwrap().0;
+
+    to_visit.push_back((0, 0));
+
+    while let Some(pos) = to_visit.pop_front() {
+        if pos == *oxygen_pos {
+            break;
+        }
+        for dir in 1..5 {
+            let new_pos = get_new_position(dir, &pos);
+            if map.contains_key(&new_pos)
+                && *map.get(&new_pos).unwrap() != Tile::Wall
+                && !prev.contains_key(&new_pos)
+            {
+                prev.insert(new_pos, pos);
+                to_visit.push_front(new_pos);
+            }
+        }
+    }
+
+    let mut backtrack = *oxygen_pos;
+    let mut length = 0;
+
+    while let Some(node) = prev.get(&backtrack) {
+        length += 1;
+        if *node == (0, 0) {
+            break;
+        }
+
+        backtrack = *node;
+    }
+
+    println!("{}", length);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Provide filename");
+        return;
+    }
+    let filepath = &args[1];
+    let input = tokenize(filepath);
+    let map = generate_map(input);
+    solve(map);
+}
